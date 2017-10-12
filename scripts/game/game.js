d929a2c940a2697526463d9914c1e09cc7531643
joannan <joannan@consol.de>
2017-10-12 20:03:44 +0200

    createGame function in game.js

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -233,6 +233,7 @@ playState.prototype = {
         this.hideTextField();
     },
 
+
     initPhysics: function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.enable(this.ballSprite);
@@ -435,12 +436,15 @@ playState.prototype = {
 // The first two arguments are the width and the height of the canvas element. In this case 640 x 480 pixels. You can resize this in the gameProperties object above.
 // The third argument is the renderer that will be used. Phaser.AUTO is used to automatically detect whether to use the WebGL or Canvas renderer.
 // The fourth argument is 'gameDiv', which is the id of the DOM element we used above in the index.html file where the canvas element is inserted.
-var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameCanvas');
+window.createGame = function(scope, players, mapIdm injector){
+
+  game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
 
 // Here we declare and add a state to the game object.
 // The first argument is the state name that will is used to switch between states
 // The second argument is the object name that will used when a state name is called
-game.state.add('main', playState);
-game.state.add('MainMenu', menuState);
+  game.state.add('main', playState);
+  game.state.add('MainMenu', menuState);
 // We are using the 'main' state name as the argument to load our new state.
-game.state.start('MainMenu');
+  game.state.start('MainMenu');
+};
