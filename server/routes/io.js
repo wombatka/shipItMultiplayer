60a8ec320d6686e0b53430d55f9d77d348db2498
maddie <midzik@consol.de>
2017-10-12 19:41:24 +0200

    Server + chat

---
+++ server/routes/io.js
@@ -0,0 +1,46 @@
+module.exports = (function(app, io) {
+
+  // Game data
+  var g = {
+    io: io,
+    players: [],
+    maps: {}
+  };
+
+  g.io.sockets.on('connection', function onConnection(socket) {
+    socket.emit('connected', { id: socket.id });
+    console.log("fjsdfisdhfiuhsdiufisudhfusdgfyusdgfysd");
+    var player = new Player({ id: socket.id });
+    g.players.push(player);
+
+    socket.on('newPlayer', onNewPlayer);
+  });
+
+  var onNewPlayer = function(data) {
+    // this.id is the socket id handling the event
+    // playerbyId is a helper function
+    var player = playerById(this.id);
+
+    // Create the map if it doesn't already exist
+    if (!g.maps[data.mapId]) {
+      g.maps[data.mapId] = new Map({id: data.mapId});
+    }
+
+    if (!player.inMap(data.mapId)) {
+      player.joinMap(g.maps[data.mapId]);
+
+      this.broadcast.to(data.mapId)
+        .emit('gameUpdated:add', {
+          player: player.serialize(),
+          map: data.mapId,
+          allPlayers: g.maps[data.mapId].players
+        });
+
+      this.join(data.mapId); // join the socket group
+      this.emit('gameUpdated:add', {
+        map: data.mapId,
+        allPlayers: g.maps[data.mapId].players
+      });
+    };
+  };
+});
---
+++ server/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var path = require("path");
+
+// Start the server
+var port = process.env.PORT || 8000;
+server.listen(port, function() {
+  console.log("Running on port ", port);
+});
+
+// Serve the client
+var staticPath = path.join(__dirname, '../client/dist');
+// app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index2.html');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+socket.on('chat message', function(msg){
+  console.log('message: ' + msg);
+
+    io.emit('chat message', msg);
+  });
+});
+
+// Handle socket.io
+//require('./routes/io.js')(app, io);
