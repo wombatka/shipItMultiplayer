fa40fffc2570096cbba019e59ede2f2350494382
joannan <joannan@consol.de>
2017-10-12 16:20:49 +0200

    basic configuration

---
+++ scripts/game/index.js
@@ -0,0 +1,14 @@
+angular.module('app.game', [])
+.config(function($stateProvider) {
+  $stateProvider
+    .state('game', {
+      url: '/game',
+      abstract: true,
+      template: '<div class="game"><div ui-view></div></div>'
+    })
+   .state('game.play', {
+      url: '/id',
+      template: '<div><div id="gameCanvas"></div></div>',
+      controller: 'GameController'
+    })
+});
